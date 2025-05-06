59b1f1ebac966527cb60f93acd7ed30ceb3a0606
Quit123 <2048751307@qq.com>
2025-05-07 07:23:45 +0800

    update Jenkinfile

--- docs-core/src/test/java/com/sismics/docs/core/dao/jpa/TestJpa.java
+++ docs-core/src/test/java/com/sismics/docs/core/dao/jpa/TestJpa.java
@@ -34,24 +34,4 @@ public class TestJpa extends BaseTransactionalTest {
         userDao.delete("testJpa", user.getId());
         TransactionUtil.commit();
     }
-
-    @Test(expected = Exception.class)
-    public void testDuplicateUsername() throws Exception {
-        UserDao userDao = new UserDao();
-
-        // Create first user
-        User user1 = new User();
-        user1.setUsername("duplicate");
-        user1.setPassword("testpass");
-        user1.setEmail("user1@docs.com");
-        userDao.create(user1, "system");
-        TransactionUtil.commit();
-
-        // Try to create user with same username
-        User user2 = new User();
-        user2.setUsername("duplicate");
-        user2.setPassword("testpass");
-        user2.setEmail("user2@docs.com");
-        userDao.create(user2, "system"); // This should throw Exception
-    }
 }
